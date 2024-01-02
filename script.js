2957a214a7a572cb95c37b6459140af1a669a113
Rakshith <rakshithrkumar9161@gmail.com>
2024-01-02 09:40:42 +0530

    added age logic

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ function darkmode(el) {
         el.value = "Switch to Dark Mode";
     }
 }
+
+var birthDate = 31;
+var birthMon = 8;
+var birthYear = 2000;
+
+var date = new Date();
+var thisDate = date.getDate();
+var thisMon = date.getMonth() + 1;
+var thisYear = date.getFullYear();
+
+var difYear = thisYear - birthYear;
+var difMon = thisMon - birthMon;
+var difDate = thisDate - birthDate;
+
+if(difDate < 0){
+    difDate += 30;
+    difMon--;
+}
+
+if(difMon < 0){
+    difMon += 12;
+    difYear--;
+}
+
+var age = document.getElementById('age');
+age.innerHTML = difYear + " Years " + difMon + " Months " + difDate + " Days.";
